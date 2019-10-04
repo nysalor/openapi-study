fd1d3410896a4a23647b6f947dff28c701296737
Jun Yokoyama <jun@larus.org>
2019-10-04 16:13:49 +0900

    ビジネスロジックを実装した

--- server/go/api_default.go
+++ server/go/api_default.go
@@ -11,15 +11,41 @@ package openapi
 
 import (
 	"net/http"
+	"os"
+	"io/ioutil"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 // GetPhotos - 
 func GetPhotos(c *gin.Context) {
-	var photos []PhotoProperties
+	imageDir := os.Getenv("IMAGEDIR")
+	photos := loadFiles(imageDir)
 	res := Photos{
 		Photos: photos,
 	}
 	c.JSON(http.StatusOK, res)
+}
+
+func loadFiles(dir string) (photos []PhotoProperties) {
+	baseUrl := os.Getenv("BASEURL")
+	id := 1
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		if !file.IsDir() && strings.HasSuffix(file.Name(), ".jpg") {
+			pp := PhotoProperties{
+				Id: id,
+				Title: file.Name(),
+				Url: baseUrl + "/" + file.Name(),
+			}
+			photos = append(photos, pp)
+			id += 1
+		}
+	}
+	return
 }
